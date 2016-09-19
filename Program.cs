887a31dc3f89e9026e8d6613a90a13bb18bfdb91
Brad Kilkenney <bkilkenney@gmail.com>
2016-09-19 16:35:05 -0400

    Slide 22 homework done

--- Program.cs
+++ Program.cs
@@ -10,6 +10,30 @@ namespace DictionariesSlide22
     {
         static void Main(string[] args)
         {
+            Dictionary<string, int> animals = new Dictionary<string, int>();
+            
+            animals.Add("Bear", 4);
+            animals.Add("Squid", 10);
+            animals.Add("Bird", 2);
+            animals.Add("Dog", 4);
+            animals.Add("Human", 2);
+            animals.Add("Marsupial", 2);
+
+            //A true statement
+            if (animals.ContainsKey("Bear"))
+            {
+                Console.WriteLine(true);
+            }
+
+            //An animal that is not in our dictionary.  Added else for false otherwise nothing prints. 
+            if (animals.ContainsKey("Dragon"))
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }           
         }
     }
 }
